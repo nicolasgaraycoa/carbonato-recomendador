8f2de4a25b91633e6e127c46df9e4f8166301ea3
nicolasgaraycoa <62861550+nicolasgaraycoa@users.noreply.github.com>
2023-06-24 13:13:34 -0500

    Update carbonato_recomendador.py

--- carbonato_recomendador.py
+++ carbonato_recomendador.py
@@ -16,7 +16,7 @@ salinidad = st.sidebar.number_input('Salinidad: ', min_value = 0,
                                     max_value= 40, value = 20)
 
 recambio = st.sidebar.number_input('% Recambio diario: ', min_value = 0, 
-                                    max_value= 100, value=10)
+                                    max_value= 99, value=10)
 
 alcalinidad = st.sidebar.number_input('Alcalinidad: ', min_value = 0, 
                                     max_value= 250, value= f0(salinidad))
@@ -25,53 +25,74 @@ alcalinidad = st.sidebar.number_input('Alcalinidad: ', min_value = 0,
 st.subheader("Aplicación de carbonato")
 
 @st.cache_data
-def reorder(a, r):
+def app_corr(a,r,s):
     r = r/100
-    dosis = range(50,200,50)
 
-    fmax = []
-
-    for i in dosis:
-        i = int(i)
-        ax = (i/10)+a
-        freq = 0
-        if a>=180:
-            fmax.append(30)
-        elif ax>180:
-            while ax>180:
-                ax = ((1-r)*ax)+(r*a)
-                freq += 1
-            fmax.append(freq)
-        else:
-            fmax.append(0)
-
-    
-    fmin = []
-    
-    for i in dosis:
-        i = int(i)
-        ax = (i/10)+a
-        freq = 0
-        if a>=130 :
-            fmin.append(30)
-        elif ax>130:
-            while ax>130:
-                ax = ((1-r)*ax)+(r*a)
-                freq += 1
-            fmin.append(freq)
-        else:
-            fmin.append(0)
+    dosis = [float(x)*0.1 for x in range(50,200,25)]
+    a_obj = f0(s)*1.6
 
 
-    resp = pd.DataFrame({
-        'dosis/ha': dosis,
-        'frecuencia_baja': fmin,
-        'frecuencia_alta': fmax
-    })
+    if a>=a_obj:
+        correctivo = pd.DataFrame({
+            'kg/ha': 0,
+            'frecuencia': np.NaN
+        }, index=[0])
+    else:
+        kgx = []
+        lfx = []
+        for i in dosis:
+            a_rpt = a_obj-(i*(1-r))
+            kgx.append(i*10)
+            ax = a
+            fx=0
+            while ax<a_rpt:
+                ax = ((ax+i)*(1-r))+(a*r)
+                fx +=1
+            lfx.append(fx)
+        correctivo = pd.DataFrame({
+            'kg/ha': kgx,
+            'frecuencia': lfx
+        }, index=range(0,len(dosis)))
+    return(correctivo)
 
-    return(resp)
+correctivo = app_corr(alcalinidad, recambio, salinidad)
+
+@st.cache_data
+def app_mant(a,r,s):
+    r = r/100
+
+    dosis = 100*0.1
+    a_obj = f0(s)*1.6
 
 
-st.dataframe(reorder(alcalinidad, recambio), hide_index=True)
-st.text('0 = aplicacion todos los dias')
+    if a>=a_obj:
+        mantenimiento = pd.DataFrame({
+            'kg/ha': dosis*10,
+            'frecuencia': 30
+        }, index =[0])
+    else:
+        a_rpt = a_obj-(dosis*(1-r))
+        ay = a_rpt+dosis
+        fy = 0
+        while ay> a_rpt:
+            ay = (ay*(1-r))+(a*r)
+            fy += 1
+        mantenimiento = pd.DataFrame({
+            'kg/ha' : dosis*10,
+            'frecuencia': fy
+        }, index=[0])
+    return(mantenimiento)
+
+mantenimiento = app_mant(alcalinidad, recambio, salinidad)
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.text('Dosis correctiva')
+    st.dataframe(correctivo)
+
+with col2:
+    st.text('Dosis mantenimiento')
+    st.dataframe(mantenimiento)
 
